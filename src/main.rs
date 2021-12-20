0b31aea665c285db98d64afddc9e77147e4c99e6
Bryon Smith <bryon@smithauto.us>
2021-12-20 15:44:52 -0600

    area function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let width1 = 30;
+    let height1 = 50;  //golden ratio 50 / 30 = 1.666666
+
+    println!("The area of the rectangle is {} square pixels.", area(width1, height1));
+}
+fn area(x:u32, y:u32) -> u32 {
+    x * y
 }
