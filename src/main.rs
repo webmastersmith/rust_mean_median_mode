9d9eabbef6d07ca9b59a52810de05c4e64768b57
Bryon Smith <bryon@smithauto.us>
2021-12-20 16:13:49 -0600

    using structs to refactor code

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 fn main() {
-    let rec1 = Rectangle {
+    let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
-    println!("The area of the rectangle is {} square pixels.", area(&rec1));
+    dbg!(rect1);
 }
-fn area(dimensions: &Rectangle) -> u32 {
-    dimensions.width * dimensions.height
-}
+// fn area(dimensions: &Rectangle) -> u32 {
+//     dimensions.width * dimensions.height
+// }
