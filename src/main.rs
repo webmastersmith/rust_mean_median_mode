9e139da2765fa197abbddb3610dc63d20461f200
Bryon Smith <bryon@smithauto.us>
2021-12-20 19:48:06 -0600

    multiple implementations

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
-#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.area() > other.area()
+    }
 }
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
-    dbg!(rect1);
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect3));
 }
-// fn area(dimensions: &Rectangle) -> u32 {
-//     dimensions.width * dimensions.height
-// }
